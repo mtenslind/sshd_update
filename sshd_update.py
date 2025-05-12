c2c228de5d6ef4b82be848e69d1d8da08eb4a120
mtenslin <mait.tenslind@ut.ee>
2025-05-12 15:56:01 +0300

    paramiko connection function

--- sshd_update.py
+++ sshd_update.py
@@ -2,6 +2,8 @@
 
 import argparse
 import os
+import paramiko
+import getpass
 
 parser = argparse.ArgumentParser(
     prog="sshd_update.py",
@@ -32,12 +34,27 @@ def parse_config_array(config_array):
     parsed_list = [i for i in config_array if not(i.startswith("#")) and i != "\n"]
     return parsed_list
 
+def paramiko_connect(host, user):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    try:
+        client.connect(host, port=22, username=user)
+    except:
+        user_password = getpass.getpass(f"Key failed, insert password for {user}: ")
+        client.connect(host, port=22, username=user, password=user_password)
+    return client
+
 def main():
     with open(args.config_file, 'r') as template_file:
         parameter_list = template_file.readlines()
 
     parsed_list = parse_config_array(parameter_list)
 
-    print(parsed_list)
+    connection = paramiko_connect(args.host, args.user)
+    (stdin, stdout, stderr) = connection.exec_command("whoami")
+    output = stdout.read()
+    print(output)
+
+    connection.close()
 
 main()
